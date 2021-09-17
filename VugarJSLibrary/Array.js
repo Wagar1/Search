24f1f7c00d90f9fc17f08237852a644f2205c688
Wagar1 <vuqar.eyvazov.1997@gmail.com>
2021-09-17 16:20:55 +0400

    go

---
+++ VugarJSLibrary/Array.js
@@ -0,0 +1,46 @@
+Array.prototype.contains = function(v) {
+    for (var i = 0; i < this.length; i++) {
+      if (this[i] === v) return true;
+    }
+   return false;
+ };
+ 
+ Array.prototype.unique = function() {
+   var arr = [];
+   for (var i = 0; i < this.length; i++) {
+     if (!arr.contains(this[i])) {
+       arr.push(this[i]);
+     }
+   }
+   return arr;
+ }
+ 
+ if (!Array.prototype.filter)
+ {
+   Array.prototype.filter = function(fun /*, thisp */)
+   {
+     "use strict";
+ 
+     if (this === void 0 || this === null)
+       throw new TypeError();
+ 
+     var t = Object(this);
+     var len = t.length >>> 0;
+     if (typeof fun !== "function")
+       throw new TypeError();
+ 
+     var res = [];
+     var thisp = arguments[1];
+     for (var i = 0; i < len; i++)
+     {
+       if (i in t)
+       {
+         var val = t[i]; // in case fun mutates this
+         if (fun.call(thisp, val, i, t))
+           res.push(val);
+       }
+     }
+ 
+     return res;
+   };
+ }
